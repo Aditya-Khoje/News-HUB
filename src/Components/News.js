8c0314ff32e4d600ed7799ac69e1657243fcece0
Aditya-Khoje <adityakhoje1@gmail.com>
2023-09-30 19:23:25 +0530

    10

--- src/Components/News.js
+++ src/Components/News.js
@@ -170,6 +170,9 @@ export class News extends Component {
                       }
                       imageUrl={element.urlToImage}
                       newsUrl={element.url}
+                      author={element.author}
+                      date={element.publishedAt}
+                      source={element.source.name}
                     />
                   </div>
                 );
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,10 +2,17 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description, imageUrl, newsUrl } = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props;
     return (
       <>
         <div className="card">
+          <span
+            className="position-absolute top-0 translate-middle badge rounded-pill bg-danger"
+            style={{ left: "85%", zIndex: "1" }}
+          >
+            {source}
+          </span>
           <img
             src={
               imageUrl
@@ -18,6 +25,12 @@ export class NewsItem extends Component {
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
+            <p className="card-text">
+              <small className="text-muted">
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
             <a
               href={newsUrl}
               target="_blank"
